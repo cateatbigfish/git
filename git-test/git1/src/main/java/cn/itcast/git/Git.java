b1b7c16e20b510cbed7c9abec3a4f86ac1ab7195
江坤 <1061550743@qq.com>
2019-06-01 21:25:50 +0800

    提交

---
+++ git-test/git1/src/main/java/cn/itcast/git/Git.java
@@ -0,0 +1,12 @@
+package cn.itcast.git;
+
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("aaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaa");
+        System.out.println("aaaaaaaaaaaaa");
+    }
+}
