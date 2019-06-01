b2bca21515810ca843cf936495eac2e933300910
江坤 <1061550743@qq.com>
2019-06-01 20:50:31 +0800

    提交

---
+++ git/src/main/java/cn/itcast/git/Git.java
@@ -0,0 +1,20 @@
+package cn.itcast.git;
+
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("hello , git");
+        System.out.println("hello , git");
+        System.out.println("hello , git");
+        System.out.println("hello , git");
+        System.out.println("hello , git");
+        System.out.println("hello , git");
+        System.out.println("hello , git");
+        System.out.println("hello , git");
+        System.out.println("hello , git");
+        System.out.println("hello , git");
+        System.out.println("hello , git");
+        System.out.println("hello , git");
+        System.out.println("hello , git");
+        System.out.println("hello , git");
+    }
+}
